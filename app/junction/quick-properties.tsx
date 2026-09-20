41767d30525809e86bba759ad4597c111172ca5a
bokoboss <188425595+bokoboss@users.noreply.github.com>
2026-09-21 00:08:50 +0700

    Remove unused audit-pass imports

--- app/junction/quick-properties.tsx
+++ app/junction/quick-properties.tsx
@@ -1,4 +1,4 @@
-import {type Design,type Arm,type Direction,type LaneRole,type LaneArrowCode,sectionFor,pocketsFor,roadsideFor,medianTreeDefaults,options,laneArrowFor,withLaneArrow,markingsFor} from './model';
+import {type Design,type Arm,type LaneRole,type LaneArrowCode,sectionFor,pocketsFor,roadsideFor,medianTreeDefaults,options,laneArrowFor,withLaneArrow,markingsFor} from './model';
 import {allocate,allocationMode} from './allocation';
 import {armMouth} from './geometry';
 import {roundSettings} from './roundabout';
